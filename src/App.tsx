971e5a0d979cf651c611b778dc8dcf8392bd9130
Shaheen Karodia <shaheen.karodia.dev@gmail.com>
2024-12-07 21:15:16 +0200

    Implemented design system colors

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,14 @@ function App() {
         P4$5W0rD! Character Length Include Uppercase Letters Include Lowercase
         Letters Include Numbers Include Symbols Strength Generate
       </div>
+      <div className="w-7 h-5 bg-1-red" />
+      <div className="w-7 h-5 bg-2-orange" />
+      <div className="w-7 h-5 bg-3-yellow" />
+      <div className="w-7 h-5 bg-neon-green" />
+      <div className="w-7 h-5 bg-almost-white" />
+      <div className="w-7 h-5 bg-grey" />
+      <div className="w-7 h-5 bg-dark-grey" />
+      <div className="w-7 h-5 bg-very-dark-grey" />
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      "1-red": "F64A4A",
+      "2-orange": "#FB7C58",
+      "3-yellow": "#F8CD65",
+      "neon-green": "#A4FFAF",
+      "almost-white": "#E6E5EA",
+      grey: "#817D92",
+      "dark-grey": "#24232C",
+      "very-dark-grey": "#18171F",
+    },
     extend: {},
   },
   plugins: [],
